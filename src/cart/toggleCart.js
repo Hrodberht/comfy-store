265b4a55acc9adedf971934b510fd4155df16886
Hrodberht <hrodberht.stefanescu@gmail.com>
2022-10-10 08:54:29 +0100

    Selected cart DOM elements

--- src/cart/toggleCart.js
+++ src/cart/toggleCart.js
@@ -1,3 +1,7 @@
-import { getElement } from '../utils.js';
+import { getElement } from "../utils.js";
+
+const cartOverlay = getElement(".cart-overlay");
+const closeCartBtn = getElement(".cart-close");
+const toggleCartBtn = getElement(".toggle-cart");
 
 export const openCart = () => {};
