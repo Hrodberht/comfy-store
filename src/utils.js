ceaaaa6d712cff33d3e27258284d58c9ad4de71f
Hrodberht <hrodberht.stefanescu@gmail.com>
2022-10-11 09:16:03 +0100

    Added getStorageItem functionality

--- src/utils.js
+++ src/utils.js
@@ -14,7 +14,16 @@ const getElement = (selection) => {
 
 const formatPrice = () => {};
 
-const getStorageItem = () => {};
+const getStorageItem = (item) => {
+  let storageItem = localStorage.getItem(item);
+  if (storageItem) {
+    storageItem = JSON.parse(localStorage.getItem(item));
+  } else {
+    storageItem = [];
+  }
+  return storageItem;
+};
+
 const setStorageItem = (name, item) => {
   localStorage.setItem(name, JSON.stringify(item));
 };
