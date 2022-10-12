7efcadcae11600db634307f7a7df3c97fe9cef48
Hrodberht <hrodberht.stefanescu@gmail.com>
2022-10-12 19:27:56 +0100

    Added more functionality to formatPrice

--- src/utils.js
+++ src/utils.js
@@ -16,7 +16,7 @@ const formatPrice = (price) => {
   let formattedPrice = new Intl.NumberFormat("en-US", {
     style: "currency",
     currency: "USD",
-  });
+  }).format((price / 100).toFixed(2));
   return formattedPrice;
 };
 
